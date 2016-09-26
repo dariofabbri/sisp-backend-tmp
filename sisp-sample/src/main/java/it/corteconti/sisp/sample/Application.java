610aa7e0deaf646da8147002a3bd4ea77ed95769
Dario Fabbri <dar.fabbri@gmail.com>
2016-09-26 18:22:19 +0200

    Sistemazione della configurazione di Swagger2

--- sisp-sample/src/main/java/it/corteconti/sisp/sample/Application.java
+++ sisp-sample/src/main/java/it/corteconti/sisp/sample/Application.java
@@ -4,8 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
@@ -16,7 +23,25 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 public class Application {
 
 	public static void main(String[] args) {
-		 
+
 		SpringApplication.run(Application.class, args);
 	}
+
+	@Bean
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.apiInfo(apiInfo())
+				.select()
+				.paths(PathSelectors.regex("/api.*"))
+				.build();
+	}
+	
+	private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Sample REST API")
+                .description("API REST di esempio")
+                .contact(new Contact("Engineering Ingegneria Informatica", "www.eng.it", null))
+                .version("0.0.1")
+                .build();
+    }
 }
