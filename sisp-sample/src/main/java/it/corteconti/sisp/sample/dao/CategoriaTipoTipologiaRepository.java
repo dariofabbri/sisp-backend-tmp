eae996d337e8dc96a991c72e959ac95458671b60
carmelonisti <senmut72@gmail.com>
2016-09-27 16:05:32 +0200

    Query recupero lista categorie in base a sezione/livello aoo

--- sisp-sample/src/main/java/it/corteconti/sisp/sample/dao/CategoriaTipoTipologiaRepository.java
+++ sisp-sample/src/main/java/it/corteconti/sisp/sample/dao/CategoriaTipoTipologiaRepository.java
@@ -1,11 +1,16 @@
 package it.corteconti.sisp.sample.dao;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
-import it.corteconti.sisp.sample.model.Categoria;
 import it.corteconti.sisp.sample.model.CategoriaTipoTipologia;
-import it.corteconti.sisp.sample.model.Giudizio;
 
 public interface CategoriaTipoTipologiaRepository extends CrudRepository<CategoriaTipoTipologia, Long> {
+	
+	
+	@Query("FROM CategoriaTipoTipologia ctt JOIN FETCH ctt.categoria c, JOIN FETCH ctt.ambito WHERE ctt.ambito.codiceAmbito=?1 AND ctt.codiceLivelloAoo=?2")
+	public List<CategoriaTipoTipologia> findByIdAmbitoAndLivelloAoo(String idAmbito, String livelloAoo);
 	
 }
