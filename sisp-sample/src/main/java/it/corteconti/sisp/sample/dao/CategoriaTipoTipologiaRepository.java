a578017dc7eeff30f42bc766a7c62fe40bcdf20c
carmelonisti <senmut72@gmail.com>
2016-09-27 16:07:30 +0200

    Correzione query

--- sisp-sample/src/main/java/it/corteconti/sisp/sample/dao/CategoriaTipoTipologiaRepository.java
+++ sisp-sample/src/main/java/it/corteconti/sisp/sample/dao/CategoriaTipoTipologiaRepository.java
@@ -10,7 +10,7 @@ import it.corteconti.sisp.sample.model.CategoriaTipoTipologia;
 public interface CategoriaTipoTipologiaRepository extends CrudRepository<CategoriaTipoTipologia, Long> {
 	
 	
-	@Query("FROM CategoriaTipoTipologia ctt JOIN FETCH ctt.categoria c, JOIN FETCH ctt.ambito WHERE ctt.ambito.codiceAmbito=?1 AND ctt.codiceLivelloAoo=?2")
+	@Query("FROM CategoriaTipoTipologia ctt JOIN FETCH ctt.categoria c, JOIN FETCH ctt.ambito a WHERE a.codiceAmbito=?1 AND ctt.codiceLivelloAoo=?2")
 	public List<CategoriaTipoTipologia> findByIdAmbitoAndLivelloAoo(String idAmbito, String livelloAoo);
 	
 }
