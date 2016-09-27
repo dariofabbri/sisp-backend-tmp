14dec6234e4e0a11138e78946c7a1bac906ff664
Mirco Marmo <mir.marmo@gmail.com>
2016-09-27 15:44:39 +0200

    dto categoria

---
+++ sisp-sample/src/main/java/it/corteconti/sisp/sample/dto/CategoriaArrayDto.java
@@ -0,0 +1,15 @@
+package it.corteconti.sisp.sample.dto;
+
+public class CategoriaArrayDto {
+	
+	private CategoriaDto[] listaCategorie;
+
+	public CategoriaDto[] getListaCategorie() {
+		return listaCategorie;
+	}
+
+	public void setArray(CategoriaDto[] listaCategorie) {
+		this.listaCategorie = listaCategorie;
+	}
+
+}
---
+++ sisp-sample/src/main/java/it/corteconti/sisp/sample/dto/CategoriaDto.java
@@ -0,0 +1,30 @@
+package it.corteconti.sisp.sample.dto;
+
+public class CategoriaDto {
+
+	private String idCategoria;
+	private String descrizioneCategoria;
+	private String ambito;
+	
+	public String getIdCategoria() {
+		return idCategoria;
+	}
+	public void setIdCategoria(String idCategoria) {
+		this.idCategoria = idCategoria;
+	}
+	public String getDescrizioneCategoria() {
+		return descrizioneCategoria;
+	}
+	public void setDescrizioneCategoria(String descrizioneCategoria) {
+		this.descrizioneCategoria = descrizioneCategoria;
+	}
+	public String getAmbito() {
+		return ambito;
+	}
+	public void setAmbito(String ambito) {
+		this.ambito = ambito;
+	}
+	
+	
+	
+}
