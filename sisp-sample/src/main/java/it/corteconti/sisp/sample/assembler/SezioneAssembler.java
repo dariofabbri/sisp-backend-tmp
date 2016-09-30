512f876592b6cb7af320e7be452ca0a1a49b6665
Bruno Borreale <Bruno Borreale@161.27.61.192>
2016-09-30 14:53:27 +0200

    javadoc assembler

--- sisp-sample/src/main/java/it/corteconti/sisp/sample/assembler/SezioneAssembler.java
+++ sisp-sample/src/main/java/it/corteconti/sisp/sample/assembler/SezioneAssembler.java
@@ -3,8 +3,17 @@ package it.corteconti.sisp.sample.assembler;
 import it.corteconti.sisp.sample.dto.SezioneDto;
 import it.corteconti.sisp.sample.model.Sezione;
 
+/**
+ * Assembler Entità <em>it.corteconti.sisp.sample.model.Sezione</em>
+ * @version 1.0
+ */
 public class SezioneAssembler {
 
+	/**
+	 * Ritorna un dto <em>it.corteconti.sisp.sample.dto.SezioneDto</em>
+	 * @param tipo	Entità <em>it.corteconti.sisp.sample.model.Sezione</em>
+	 * @return 		<em>it.corteconti.sisp.sample.dto.SezioneDto</em>
+	 */
 	public static SezioneDto assembleDto(Sezione sezione) {
 		
 		SezioneDto dto = new SezioneDto();
--- sisp-sample/src/main/java/it/corteconti/sisp/sample/assembler/TipologiaAssembler.java
+++ sisp-sample/src/main/java/it/corteconti/sisp/sample/assembler/TipologiaAssembler.java
@@ -3,8 +3,18 @@ package it.corteconti.sisp.sample.assembler;
 import it.corteconti.sisp.sample.dto.TipologiaDto;
 import it.corteconti.sisp.sample.model.Tipologia;
 
+/**
+ * Assembler Entità <em>it.corteconti.sisp.sample.model.Tipologia</em>
+ * @version 1.0
+ */
+
 public class TipologiaAssembler {
 
+	/**
+	 * Ritorna un dto <em>it.corteconti.sisp.sample.dto.TipologiaDto</em>
+	 * @param tipo	Entità <em>it.corteconti.sisp.sample.model.Tipologia</em>
+	 * @return 		<em>it.corteconti.sisp.sample.dto.TipologiaDto</em>
+	 */
 	public static TipologiaDto assembleDto(Tipologia tipologia) {
 		TipologiaDto tipologiaDto = new TipologiaDto();
 		
