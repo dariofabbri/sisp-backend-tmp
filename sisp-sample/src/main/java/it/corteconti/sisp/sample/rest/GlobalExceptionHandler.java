f48b3682696c3c6418b3f77055781c0385b2a9ad
carmelonisti <senmut72@gmail.com>
2016-10-05 16:40:04 +0200

    Ripristino situazione precedente

--- sisp-sample/src/main/java/it/corteconti/sisp/sample/rest/GlobalExceptionHandler.java
+++ sisp-sample/src/main/java/it/corteconti/sisp/sample/rest/GlobalExceptionHandler.java
@@ -1,5 +1,12 @@
 package it.corteconti.sisp.sample.rest;
 
+import it.corteconti.sisp.sample.dto.ErrorDto;
+import it.corteconti.sisp.sample.dto.ValidationErrorDto;
+import it.corteconti.sisp.sample.exception.ResourceNotFoundException;
+import it.corteconti.sisp.sample.exception.ValidationException;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
@@ -7,11 +14,6 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
-
-import it.corteconti.sisp.sample.dto.ErrorDto;
-import it.corteconti.sisp.sample.dto.ValidationErrorDto;
-import it.corteconti.sisp.sample.exception.ResourceNotFoundException;
-import it.corteconti.sisp.sample.exception.ValidationException;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
@@ -21,7 +23,7 @@ public class GlobalExceptionHandler {
 	@ResponseStatus(HttpStatus.NOT_FOUND)
 	@ExceptionHandler(ResourceNotFoundException.class)
 	@ResponseBody ErrorDto
-	handleNotFound(Exception ex) {
+	handleNotFound(HttpServletRequest req, Exception ex) {
 
 		LOG.error("Risorsa non trovata.", ex);
 	    return new ErrorDto(ex.getMessage());
@@ -30,7 +32,7 @@ public class GlobalExceptionHandler {
 	@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
 	@ExceptionHandler(RuntimeException.class)
 	@ResponseBody ErrorDto
-	handleInternalServerError(Exception ex) {
+	handleInternalServerError(HttpServletRequest req, Exception ex) {
 
 		LOG.error("Rilevata una runtime exception non gestita.", ex);
 	    return new ErrorDto(ex.getMessage());
@@ -39,7 +41,7 @@ public class GlobalExceptionHandler {
 	@ResponseStatus(HttpStatus.BAD_REQUEST)
 	@ExceptionHandler(ValidationException.class)
 	@ResponseBody ValidationErrorDto
-	handleBadRequest(ValidationException ex) {
+	handleBadRequest(HttpServletRequest req, ValidationException ex) {
 
 		LOG.error("Errore di validazione rilevato.", ex);
 		
