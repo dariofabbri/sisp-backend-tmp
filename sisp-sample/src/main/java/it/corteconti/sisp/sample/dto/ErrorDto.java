2d435201916d4ce9998cb02ed318cb47e46cb997
carmelonisti <senmut72@gmail.com>
2016-10-05 18:10:01 +0200

    Modifiche segnalazioni Sonar

--- sisp-sample/src/main/java/it/corteconti/sisp/sample/dto/ErrorDto.java
+++ sisp-sample/src/main/java/it/corteconti/sisp/sample/dto/ErrorDto.java
@@ -3,8 +3,10 @@ package it.corteconti.sisp.sample.dto;
 public class ErrorDto {
 
 	private String message;
-
+	
+	
 	public ErrorDto() {
+		// -- Costruttore di default
 	}
 	
 	public ErrorDto(String message) {
