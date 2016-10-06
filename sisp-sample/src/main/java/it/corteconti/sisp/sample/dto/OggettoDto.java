6db4d3d566090bbb1e5dfab14625676f44113f30
Mirco Marmo <mir.marmo@gmail.com>
2016-10-06 17:13:07 +0200

    creati metodi di test di esempio

--- sisp-sample/src/main/java/it/corteconti/sisp/sample/dto/OggettoDto.java
+++ sisp-sample/src/main/java/it/corteconti/sisp/sample/dto/OggettoDto.java
@@ -2,28 +2,38 @@ package it.corteconti.sisp.sample.dto;
 
 import org.apache.commons.lang3.builder.ToStringBuilder;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
 /**
- * <p>Dto che rappresenta un oggetto di tipo Oggetto<p>
+ * Dto relativo alla tabella <em>OGGETTO</em>
  * @version 1.0
- *
  */
+@ApiModel
 public class OggettoDto {
 
 	private Long idOggetto;
 	private String descrizioneOggetto;
 	
+	
+	@ApiModelProperty(position = 1, required = false)
 	public Long getIdOggetto() {
 		return idOggetto;
 	}
+	
 	public void setIdOggetto(Long idOggetto) {
 		this.idOggetto = idOggetto;
 	}
+
+	@ApiModelProperty(position = 2, required = false)
 	public String getDescrizioneOggetto() {
 		return descrizioneOggetto;
 	}
+	
 	public void setDescrizioneOggetto(String descrizioneOggetto) {
 		this.descrizioneOggetto = descrizioneOggetto;
 	}
+	
 	
 	@Override
 	public String toString() {
@@ -32,7 +42,6 @@ public class OggettoDto {
 				.append("descrizioneOggetto", descrizioneOggetto)
 				.toString();
 	}
-	
 	
 	
 }
--- sisp-sample/src/test/java/it/corteconti/sisp/sample/rest/ThingResourceTest.java
+++ sisp-sample/src/test/java/it/corteconti/sisp/sample/rest/ThingResourceTest.java
@@ -1,10 +1,11 @@
 package it.corteconti.sisp.sample.rest;
 
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
 import java.net.URL;
+import java.util.Date;
 
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +14,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.client.RestTemplate;
 
 import it.corteconti.sisp.sample.dto.ThingDto;
 
@@ -27,19 +29,48 @@ public class ThingResourceTest {
 
     @Autowired
     private TestRestTemplate template;
-
-    @Before
-    public void setUp() throws Exception {
-        this.base = new URL("http://localhost:"+port+"/api/v1/things/1");
-    }
+    
+    @Autowired
+    private RestTemplate template2;
 
     @Test
-    public void getHello() throws Exception {
+    public void getTest() throws Exception {
+    	
+    	this.base = new URL("http://localhost:"+port+"/api/v1/things/1");
+    	
         ResponseEntity<ThingDto> response = template.getForEntity(base.toString(),ThingDto.class);
         ThingDto thingdto = response.getBody();
         assertTrue(thingdto.getDescription().equals("Pippo"));
     }
+    
+	@Test
+    public void saveUpdateDeleteTest()throws Exception {
+    	
+    	this.base = new URL("http://localhost:"+port+"/api/v1/things/thing");
+    	
+    	/*
+    	 * verifico la creazione dell'oggetto thing
+    	 */
+    	ThingDto thingDto = new ThingDto();
+    	thingDto.setDescription("Prova Test");
+    	thingDto.setLastUpdate(new Date());
+    	ThingDto thingDtoOut1 = template.postForObject(base.toString(), thingDto, ThingDto.class);
+    	assertNotNull( thingDtoOut1.getId() );
+    	
+    	/*
+    	 * verifico update dell'oggetto thing
+    	 */
+    	this.base = new URL("http://localhost:"+port+"/api/v1/things/thing/update");
+    	thingDtoOut1.setDescription("prova_test_modificata");
+    	template.put(base.toString(), thingDtoOut1);
+    	assertTrue(thingDtoOut1.getDescription().equals("prova_test_modificata"));
+    	
+    	/*
+    	 * verifico la cancellazione dell'oggetto thing
+    	 */
+    	this.base = new URL("http://localhost:"+port+"/api/v1/things/thing/delete/"+thingDtoOut1.getId());
+    	template.delete(base.toString());
+    	
+    }
 	
-	
-
 }
