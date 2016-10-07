82674ca83615933db218f23008f7bedc3b428b89
carmelonisti <senmut72@gmail.com>
2016-10-07 12:35:55 +0200

    Modifica passaggio parametri alla query (@Param)

--- sisp-sample/src/main/java/it/corteconti/sisp/sample/dao/CategoriaRepository.java
+++ sisp-sample/src/main/java/it/corteconti/sisp/sample/dao/CategoriaRepository.java
@@ -4,36 +4,37 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import it.corteconti.sisp.sample.model.Categoria;
 
 /**
  * Repository per Entità <em>it.corteconti.sisp.sample.model.Categoria</em>
  * @version 1.0
- *
  */
-
 public interface CategoriaRepository extends CrudRepository<Categoria, String> {
 	
 	/**
-	 * <p>Ritorna una lista di di oggetti Categoria<p>
-	 * @param codiceAmbito
-	 * @param codiceLivelloAoo
-	 * @return <em>java.util.List<it.corteconti.sisp.sample.model.Categoria></em>
+	 * Ritorna una lista di entità <em>it.corteconti.sisp.sample.model.Categoria</em>
+	 * @param codiceAmbito		id dell'entità Ambito
+	 * @param codiceLivelloAoo	livello AOO
+	 * @return 					<em>java.util.List<it.corteconti.sisp.sample.model.Categoria></em>
 	 */
+	@Query("SELECT DISTINCT ctt.categoria FROM CategoriaTipoTipologia ctt WHERE ctt.ambito.codiceAmbito = :codiceAmbito AND ctt.codiceLivelloAoo = :codiceLivelloAoo ORDER BY ctt.categoria.ordineCategoria ASC")
+	public List<Categoria> findFromCategoriaTipoTipologiaByIdAmbitoAndLivelloAoo(@Param("codiceAmbito") String codiceAmbito,
+																				 @Param("codiceLivelloAoo") String codiceLivelloAoo);
 	
-	@Query("SELECT DISTINCT ctt.categoria FROM CategoriaTipoTipologia ctt WHERE ctt.ambito.codiceAmbito=?1 AND ctt.codiceLivelloAoo=?2 ORDER BY ctt.categoria.ordineCategoria ASC")
-	public List<Categoria> findFromCategoriaTipoTipologiaByIdAmbitoAndLivelloAoo(String codiceAmbito, String codiceLivelloAoo);
 	
 	/**
-	 * <p>Ritorna un oggetto di tipo Categoria, dato codice ambito, codice livello AOO e id categoria.<p>
-	 * @param codiceAmbito
-	 * @param codiceLivelloAoo
-	 * @param categoriaId
-	 * @return <em>it.corteconti.sisp.sample.model.Categoria</em>
+	 * Ritorna un'entità <em>it.corteconti.sisp.sample.model.Categoria</em>
+	 * @param codiceAmbito		id dell'entità Ambito
+	 * @param codiceLivelloAoo	livello AOO
+	 * @param categoriaId		id dell'entità Categoria
+	 * @return 					<em>it.corteconti.sisp.sample.model.Categoria</em>
 	 */
-	
-	@Query("SELECT DISTINCT ctt.categoria FROM CategoriaTipoTipologia ctt WHERE ctt.ambito.codiceAmbito=?1 AND ctt.codiceLivelloAoo=?2 AND ctt.categoria.codiceCategoria=?3 ORDER BY ctt.categoria.ordineCategoria ASC")
-	public Categoria findFromCategoriaTipoTipologiaByIdAmbitoAndLivelloAooAndIdCategoria(String codiceAmbito, String codiceLivelloAoo, String categoriaId);
+	@Query("SELECT DISTINCT ctt.categoria FROM CategoriaTipoTipologia ctt WHERE ctt.ambito.codiceAmbito = :codiceAmbito AND ctt.codiceLivelloAoo = :codiceLivelloAoo AND ctt.categoria.codiceCategoria = :codiceCategoria ORDER BY ctt.categoria.ordineCategoria ASC")
+	public Categoria findFromCategoriaTipoTipologiaByIdAmbitoAndLivelloAooAndIdCategoria(@Param("codiceAmbito") String codiceAmbito,
+																						 @Param("codiceLivelloAoo") String codiceLivelloAoo,
+																						 @Param("codiceCategoria") String codiceCategoria);
 	
 }
