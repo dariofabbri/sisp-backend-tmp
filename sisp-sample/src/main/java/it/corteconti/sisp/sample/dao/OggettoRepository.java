64557d7456254756c5359fad222633ac23d821cc
Mirco Marmo <mir.marmo@gmail.com>
2016-10-07 14:48:46 +0200

    MERGE

--- sisp-sample/src/main/java/it/corteconti/sisp/sample/dao/OggettoRepository.java
+++ sisp-sample/src/main/java/it/corteconti/sisp/sample/dao/OggettoRepository.java
@@ -4,28 +4,34 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
 import it.corteconti.sisp.sample.model.Oggetto;
+
 /**
  * Repository Entità <em>it.corteconti.sisp.sample.model.Oggetto</em>
  * @version 1.0
  */
 public interface OggettoRepository extends CrudRepository<Oggetto, Long> {
 	
+	
 	/**
 	 * <p>Cerca gli oggetti relativi ad una determinata sezione<p>
-	 * @param sezioneId
-	 * @return <em>java.util.List<it.corteconti.sisp.sample.model.Oggetto></em>
+	 * @param sezioneId	id dell'entità Sezione
+	 * @return 			<em>java.util.List<it.corteconti.sisp.sample.model.Oggetto></em>
 	 */
-	@Query("SELECT DISTINCT o FROM Oggetto o WHERE o.sezione.id=?1")
-	public List<Oggetto> findListOggettoByIdSezione(Long sezioneId);
+	@Query("SELECT DISTINCT o FROM Oggetto o WHERE o.sezione.id = :sezioneId")
+	public List<Oggetto> findListOggettoByIdSezione(@Param("sezioneId") Long sezioneId);
+	
 	
 	/**
 	 * <p>Cerca l'oggetto di una determinata sezione per codice dell'oggetto<p>
-	 * @param sezioneId
-	 * @param idOggetto
-	 * @return <em>it.corteconti.sisp.sample.model.Oggetto</em>
+	 * @param sezioneId	id dell'entità Sezione
+	 * @param oggettoId	id dell'entità Oggetto
+	 * @return 			<em>it.corteconti.sisp.sample.model.Oggetto</em>
 	 */
-	@Query("SELECT DISTINCT o FROM Oggetto o WHERE o.sezione.id=?1 AND o.codiceOggetto=?2")
-	public Oggetto findListOggettoByIdSezioneAndIdOggetto(Long sezioneId, Long idOggetto);
+	@Query("SELECT DISTINCT o FROM Oggetto o WHERE o.sezione.id = :sezioneId AND o.codiceOggetto = :oggettoId")
+	public Oggetto findOggettoByIdSezioneAndIdOggetto(@Param("sezioneId") Long sezioneId,
+													  @Param("oggettoId") Long oggettoId);
 
 }
